25d9d635ccffd6af517f6716722f5a3f44b83ff3
Gustavo Cortez <cmgustavo83@gmail.com>
2025-05-07 17:37:38 -0300

    Add copy-to-clipboard functionality for addresses
    Introduced a feature to copy addresses directly to the clipboard by clicking on them. Changed the display style for addresses and enhanced the badge color for better visual distinction. Updated the component to use both web and device-specific clipboard APIs, accompanied by user feedback via a toast notification.

--- src/app/components/addresses/addresses.component.ts
+++ src/app/components/addresses/addresses.component.ts
@@ -1,7 +1,9 @@
 import {Component, Input} from '@angular/core';
-import {IonicModule} from "@ionic/angular";
+import {IonicModule, Platform} from "@ionic/angular";
 import {NgForOf, NgIf} from "@angular/common";
 import {Address, WalletService} from "../../services/wallet/wallet.service";
+import {Clipboard} from "@capacitor/clipboard";
+import {ToastService} from "../../services/toast/toast.service";
 
 @Component({
   selector: 'app-addresses-component',
@@ -16,7 +18,20 @@ import {Address, WalletService} from "../../services/wallet/wallet.service";
 })
 export class AddressesComponent {
   @Input() addresses: Address[] | undefined;
+  public isDevice = this.platform.is('capacitor');
 
-  constructor(public walletService: WalletService) {
+  constructor(public walletService: WalletService, public platform: Platform, private toastService: ToastService) {
+  }
+
+  public copyAddress = async (address: string) => {
+    if (address) {
+      if (this.isDevice) {
+        await Clipboard.write({string: address});
+      } else {
+        await navigator.clipboard.writeText(address);
+      }
+      console.log('Copied address: ', address);
+      await this.toastService.presentToast('Copied to clipboard!');
+    }
   }
 }
